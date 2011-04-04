6d0d19b6e1e5a1d35af0b4776082438a13753b20
Eric Walkingshaw <walkiner@eecs.oregonstate.edu>
2011-04-04 15:33:35 -0700

    Changing semantics to map to plain expressions.
    Ignore-this: 639eeee295ec332903c7469f36a843bf
    
    darcs-hash:20110404223335-4d841-f0dad16d325bd33854739919ff3ac2145cbb2a87.gz

--- CC/Semantics.hs
+++ CC/Semantics.hs
@@ -15,8 +15,11 @@ import CC.Pretty
 data QTag = Q Dim Tag deriving Eq
 type Dec  = [QTag]
 
--- CC semantics (can represent partial or full)
-type Sem a = Map Dec (Value a)
+-- CC semantics
+type Sem a = Map Dec a
+
+-- CC partial semantics
+type PSem a = Map Dec (Value a)
 
 -- a choice calculus value, either a closure or a plain expression
 data Value a = Plain a
@@ -24,7 +27,7 @@ data Value a = Plain a
              deriving Eq
 
 -- closure environment
-type Env a = Map Var (Sem a)
+type Env a = Map Var (PSem a)
 
 value :: Value a -> V a
 value (Plain     e) = Obj e
@@ -37,22 +40,22 @@ value (Close _ v e) = Abs v e
 
 -- computing the semantics function
 sem :: (Compose a, Data a) => V a -> Sem a
-sem = vary []
+sem e = [(d,p) | (d,Plain p) <- vary [] e]
 
 -- pretty printed semantics
 psem :: (Show a, Compose a, Data a) => V a -> IO ()
 psem = putStr . showSem . sem
 
 -- partial semantics composition (bowtie in FSE-11 paper)
-compose :: (Compose a, Data a) => Sem a -> Sem a -> Sem a
+compose :: (Compose a, Data a) => PSem a -> PSem a -> PSem a
 compose sl sr = concatMap (composeR sr) sl
 
-composeR :: (Compose a, Data a) => Sem a -> (Dec, Value a) -> Sem a
+composeR :: (Compose a, Data a) => PSem a -> (Dec, Value a) -> PSem a
 composeR sr (ql,Plain l) = [(ql++qr, Plain (l <.> r)) | (qr, Plain r) <- sr]
 composeR sr (ql,Close m v r)   = [(ql++qr, vr) | (qr,vr) <- vary ((v,sr):m) r]
 
 -- partial semantics (V_rho in FSE-11 paper)
-vary :: (Compose a, Data a) => Env a -> V a -> Sem a
+vary :: (Compose a, Data a) => Env a -> V a -> PSem a
 vary m (Ref v)     = maybe (unboundVar v) id (lookup v m)
 vary m (Abs v e)   = [([], Close m v e)]
 vary m (App l r)   = vary m l `compose` vary m r
@@ -78,7 +81,7 @@ foldSem (v:vs) = [(qs++qs',e:es) | (qs,e) <- v, (qs',es) <- foldSem vs]
 
 -- choice elimination
 elim :: Data a => Dim -> Int -> V a -> V a
-elim d i   (Chc d' es)  | d == d' = es !! i
+elim d i   (Chc d' es)  | d == d' = elim d i (es !! i)
 elim d _ e@(Dim d' _ _) | d == d' = e
 elim d i e = ccT (elim d i) e
 
