62794911f8f2ab891e459d2c56685cec9c73730a
Eric Walkingshaw <walkiner@eecs.oregonstate.edu>
2011-04-04 23:45:46 -0700

    Moved examples into their own module, separated feature model and tree examples.
    Ignore-this: 40ad445645d77a6a4672faedf018f29b
    
    darcs-hash:20110405064546-4d841-35a3a72d32ac80e52f591a2eb697383d83118178.gz

---
+++ Examples/FeatureModel.hs
@@ -0,0 +1,57 @@
+module Examples.FeatureModel where
+
+import CC.Syntax
+import CC.Static
+import CC.Semantics
+import CC.LambdaCalculus
+import CC.FeatureModel
+
+
+-------------------------------
+-- Feature Modeling Examples --
+-------------------------------
+
+-- some really basic generic features
+pre  = Abs "pre"  $ Abs "x" $ Ref "pre" @@ Ref "x"
+post = Abs "post" $ Abs "x" $ Ref "x"   @@ Ref "post"
+wrap = Abs "pre"  $ Abs "post" $ Abs "x" $ Ref "pre" @@ Ref "x" @@ Ref "post"
+
+-- arbitrary data
+a = Obj "a"
+b = Obj "b"
+c = Obj "c"
+
+-- some specific features
+pre'  = pre  @@ a
+post' = post @@ c
+wrap' = wrap @@ a @@ c
+
+-- basic application of features
+ab  = pre'  @@ b
+bc  = post' @@ b
+abc = wrap' @@ b
+
+-- applying a feature multiple times
+pre4 = num 4 @@ pre' @@ bc
+
+-- apply one feature (x)or another
+preXORpost = alt2 @@ pre' @@ post' @@ b
+preORpost  = or2  @@ pre' @@ post' @@ b
+
+-- apply a feature once or twice
+prePre = opt @@ (dot @@ pre' @@ (opt @@ pre')) @@ b
+
+-- apply a feature an arbitrary number of times
+preArb  = arb @@ pre' @@ b
+preArb5 = pretty $ take 5 $ sem preArb -- print out the first five entries
+
+-- apply a feature two to four times
+pre24 = ntom 2 4 @@ pre' @@ b
+
+-- choose two to four of any feature
+any24 = ntom 2 4 @@ (alt2 @@ pre' @@ post') @@ b
+
+-- a more complicated feature model
+fm = opt @@ (alt3 @@ (dot @@ (opt @@ post') @@ pre')
+                  @@ (dot @@ (opt @@ pre' ) @@ post')
+                  @@ wrap') @@ b
